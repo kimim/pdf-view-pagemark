42a425ce87d0e1db218b9dabefa95f76f9d6742d
kimim <kimi.im@outlook.com>
2023-05-09 05:55:01 +0800

    add deps

--- pdf-view-pagemark.el
+++ pdf-view-pagemark.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/kimim/pdf-view-pagemark
 ;; Keywords: pdf reading experience
 ;; Version: 0.1
-;; Package-Requires: ((pdf-tools "0.90") (emacs "26.0"))
+;; Package-Requires: ((pdf-tools "0.90") (posframe "1.4.2") (emacs "26.0"))
 
 ;; This file is NOT part of GNU Emacs.
 
